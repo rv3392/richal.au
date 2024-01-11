df4336edeb006fac1f41af2f9cbf318be1247feb
Richal Verma <richalverma00@gmail.com>
2024-01-11 12:17:14 +1000

    Install astro vercel adapter for vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://richal.au/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://richal.au/',
+  integrations: [mdx(), sitemap()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
